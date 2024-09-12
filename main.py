0bef979a6ef853a7532c506f4b9771806d5332aa
rduong11 <raymondduong12@gmail.com>
2024-09-11 22:59:47 -0500

    encryption and decryption implementation, in all caps though

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import string
 
 #an alphabet dictionary that maps the letter to its relative position in the alphabet
-alphabet = {letter: index for index, letter in enumerate(string.ascii_uppercase, start=1)}
+#{A: , B: 2, ... Z: 26}
+alphabet = {letter: index for index, letter in enumerate(string.ascii_uppercase, start=0)}
+
+#reverse of above
+#{1: A, 2: B, ... 26: Z}
+indices = {index: letter for letter, index in alphabet.items()}
+
 
 # Encryption:
 #  Prompt the user to enter plaintext and a key.
@@ -9,16 +15,28 @@ alphabet = {letter: index for index, letter in enumerate(string.ascii_uppercase,
 #  Display the resulting ciphertext.
 
 def encryption():
-    plainText = input("Enter plaintext:")
-    #sajid
-    key = input("Enter key: ")
-    #1
+    plainText = input("Enter plaintext: ").upper()
+    key = int(input("Enter key: "))
+    cipherText = ""
+    #enumerate through plaintext and print out the letters in its positions + key shift (relative to the alphabet mapping)
 
-    #enumerate through plaintext and print out the letters in its positions + 1 (relative to the alphabet mapping)
+    for letter in plainText:
+        shift = key
+        
+        if letter in alphabet:
+            shiftedPosition = (alphabet[letter] + shift) % 26  # new position is alphabet position + key; % is for the edge case of letters towards the end so that it can wrap back around
+            cipherText += indices[shiftedPosition] #add shifted letter to ciphertext
 
+            #Case-follow (QoL)
 
+            # if letter.isupper():
+            #     cipherText += shiftedLetter
+            # else:
+            #     cipherText += shiftedLetter.lower()
+        else:
+            cipherText += letter # non-alphabet characters edge case
 
-    print("Ciphertext: ")
+    print("Ciphertext: " + cipherText)
 
 # Decryption:
 #  Prompt the user to enter ciphertext and the corresponding key used for encryption.
@@ -26,7 +44,29 @@ def encryption():
 #  Display the decrypted plaintext.
 
 def decryption():
-    pass
+    #reverse of encryption but instead decrement in the shifted position. 
+    cipherText = input("Enter ciphertext: ").upper()
+    key = int(input("Enter key: "))
+    plainText = ""
+    #enumerate through plaintext and print out the letters in its positions + key shift (relative to the alphabet mapping)
+
+    for letter in cipherText:
+        shift = key
+        
+        if letter in alphabet:
+            shiftedPosition = (alphabet[letter] - shift) % 26  # new position is alphabet position - key; the % is for the edge case of letters towards the end so that it can wrap back around
+            plainText += indices[shiftedPosition] #add shifted letter to ciphertext
+
+            #Case-follow (QoL)
+
+            # if letter.isupper():
+            #     cipherText += shiftedLetter
+            # else:
+            #     cipherText += shiftedLetter.lower()
+        else:
+            plainText += letter # non-alphabet characters edge case
+
+    print("Ciphertext: " + plainText)
 
 
 
@@ -46,9 +86,9 @@ choice = input("Enter your choice (1/2/3): ")
 
 
 
-if choice == 1:
+if choice == "1":
     encryption()
-elif choice == 2:
+elif choice == "2":
     decryption()
 else:
     bruteForceAttack()
